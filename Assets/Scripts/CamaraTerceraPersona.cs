45bbc3642f6df5c61f8c7f98c10bfeeb04d9094d
Jeanzo1719 <sebasrios1119@gmail.com>
2026-04-27 07:02:04 -0500

    feat: script de camara programado y aplicado

--- Assets/Scripts/CamaraTerceraPersona.cs
+++ Assets/Scripts/CamaraTerceraPersona.cs
@@ -2,19 +2,35 @@ using UnityEngine;
 
 public class CamaraTerceraPersona : MonoBehaviour
 {
-    public Transform tarjet;
+    public Transform target;
 
     public float distance = 10.0f;
     public float anguloVertical = 45.0f;
+    public float anguloHorizontal = 90.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    void LateUpdate()
+    {
+        // angulo de rotación de la camara
+        Quaternion rotation = Quaternion.Euler(anguloVertical, anguloHorizontal, 0);
+
+        // la posición deseada según el objetivo
+        Vector3 offset = rotation * Vector3.back * distance;
+        Vector3 posicionDeseada = target.position + offset;
         
+        transform.position = posicionDeseada;
+
+        //obligando a la caara a mirar al objetivo
+        transform.LookAt(target.position);
     }
 }
